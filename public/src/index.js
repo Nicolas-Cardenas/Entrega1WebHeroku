ef304e84b2a1fda94ae922a1bcdb51547c3ecf61
Nicolas Cardenas <n.cardenass@uniandes.edu.co>
2021-11-08 00:32:46 -0500

    aa

--- public/src/index.js
+++ public/src/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 //import Perfil from "./components/perfil.js";
-import Registrar from "./src/components/registrarConductor";
+import Registrar from "./components/registrarConductor";
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
-import Perfil from "./src/components/perfil";
-import CrearViaje from "./src/components/crearViaje";
-import RegistrarVehiculo from "./src/components/registrarVehiculo";
-import Screen from "./src/components/mainScreen/screen";
-import ModificarVehiculo from "./src/components/modificarVehiculo";
-import BuscarViajes from "./src/components/buscarViajes";
-import Footer from "./src/components/footer";
-import HeaderScreen from "./src/components/mainScreen/headerScreen";
-import VerViajesConductor from "./src/components/verViajesConductor";
-import DetalleViaje from "./src/components/detalleViaje";
+import Perfil from "./components/perfil";
+import CrearViaje from "./components/crearViaje";
+import RegistrarVehiculo from "./components/registrarVehiculo";
+import Screen from "./components/mainScreen/screen";
+import ModificarVehiculo from "./components/modificarVehiculo";
+import BuscarViajes from "./components/buscarViajes";
+import Footer from "./components/footer";
+import HeaderScreen from "./components/mainScreen/headerScreen";
+import VerViajesConductor from "./components/verViajesConductor";
+import DetalleViaje from "./components/detalleViaje";
 
 ReactDOM.render(
   <div>
